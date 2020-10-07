15767075ee7bbf17036d66addc3ee2994fa39feb
paulanguedes <paulanguedes@gmail.com>
2020-10-07 14:44:34 -0300

    edit function, route and view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,4 +8,15 @@ class RecipesController < ApplicationController
         @recipe = Recipe.find(params[:id])
     end
 
+    def edit
+        @recipe = Recipe.find(params[:id])
+    end
+
+    def update
+        @recipe = Recipe.find(params[:id])
+        recipe_params = params.require(:recipe).permit(:name, :stuff, :calories, :prepare_mode, :cost)
+        @recipe.update(recipe_params)
+        redirect_to @recipe
+    end
+
 end
