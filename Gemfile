19c964791cb6b3cd1c4381779f2e7c12465e1a49
paulanguedes <paulanguedes@gmail.com>
2020-10-08 14:45:57 -0300

    seeder

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ end
 
 group :production do
   gem 'pg'
+  gem 'rails_12factor'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,226 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Recipe.create!([
+    {
+        name: "Fettuccine alfredo",
+        stuff: "250 g de fettuccine, 
+        1 xícara (chá) de creme de leite fresco, 
+        2 colheres (sopa) de manteiga, 
+        ⅔ de xícara (chá) de queijo parmesão ralado e 
+        sal a gosto",
+        calories: 290,
+        kind: "Massa",
+        prepare_mode: "Numa panela grande, coloque 3 litros de água e 1 colher (sopa) de sal. 
+        Leve ao fogo alto. Quando a água ferver, coloque o macarrão e deixe cozinhar conforme 
+        as instruções da embalagem. Cuidado para não deixar o macarrão cozinhar demais, 
+        ele deve ficar al dente. Enquanto o macarrão cozinha, comece a preparar o molho. Numa 
+        panela, junte a manteiga e o creme de leite fresco e leve ao fogo alto para ferver, 
+        mexendo sempre. Sem parar de mexer, deixe cozinhar por 3 minutos. Desligue o fogo e 
+        reserve. Com cuidado, despeje o macarrão cozido em um escorredor. A seguir, transfira 
+        o fettuccine para 2 pratos individuais. Com uma colher, distribua o molho bem quente 
+        sobre o macarrão, misture e polvilhe com o parmesão ralado. Sirva a seguir.",
+        portion: "2",
+        duration: "2020-10-06 00:25:00",
+        cost: 30.00,
+        poster: "Fettuccine.jpeg"
+    },
+    {
+        name: "Paillard de frango à milanesa",
+        stuff: "4 sassamis de frango, 
+        ½ xícara (chá) de farinha de trigo, 
+        ⅔ de xícara (chá) de farinha de rosca, 
+        2 ovos, 
+        1 xícara (chá) de óleo para fritar e 
+        sal e pimenta-do-reino moída na hora a gosto",
+        calories: 220,
+        kind: "Carnes",
+        prepare_mode: "Corte cada sassami ao meio, no sentido da largura - assim os paillards 
+        ficam arredondados na hora de abrir e a apresentação fica mais delicada. 
+        Para fazer os paillards: sobre a tábua, coloque um pedaço de filme, disponha 1 sassami 
+        no centro e cubra com outro pedaço de filme (se preferir, utilize um saco plástico para 
+        alimentos cortado ao meio). Com a parte mais gordinha da palma da mão (ou com um batedor 
+        de carnes), bata e, ao mesmo tempo, empurre a carne do centro para as laterais – o 
+        objetivo é deixar o paillard bem fininho. Cuidado: se bater forte demais a carne pode 
+        romper. Numa tigela pequena, quebre um ovo de cada vez e transfira para um prato fundo; 
+        bata com um garfo apenas para misturar as claras com as gemas. Num prato raso, coloque a 
+        farinha de rosca, em outro coloque a farinha de trigo e tempere com uma pitada de sal. 
+        Forre uma travessa com papel-toalha. Numa frigideira de borda alta, coloque o óleo e leve 
+        ao fogo médio para aquecer. Para saber a temperatura certa para fritar: coloque um palito 
+        de fósforo; quando acender é sinal de que está no ponto certo. Enquanto o óleo aquece, 
+        empane os paillards: tempere com sal e pimenta a gosto; passe cada paillard pela farinha 
+        de trigo e bata com as mãos para retirar o excesso; passe os sassamis pelo ovo batido e 
+        deixe escorrer bem o excesso; passe pela farinha de rosca e pressione com a mão para 
+        cobrir bem toda a superfície. Com cuidado, coloque 3 paillards para fritar de cada vez. 
+        Deixe cerca de 2 minutos até dourar e só então, com cuidado, vire com a escumadeira (ou 
+        pinça) para dourar o outro lado. Transfira para a travessa forrada com papel-toalha 
+        e repita com o restante. Sirva a seguir.",
+        portion: "3",
+        duration: "2020-10-06 00:35:00",
+        cost: 19.00,
+        poster: "Frango-a-milanesa.jpg"
+    },
+    {
+        name: "Salada de grãos",
+        stuff: "1 xícara (chá) de trigo grosso, 
+        1 xícara (chá) de quinoa, 
+        1 xícara (chá) de cevada, 
+        3 folhas de louro, 
+        1 punhado de miniagrião, 
+        6 damascos, 
+        5 castanhas-do-pará e 
+        sal grosso a gosto",
+        calories: 85,
+        kind: "Saladas",
+        prepare_mode: "Coloque o trigo, a quinoa e a cevadinha em 3 panelas diferentes. Cubra 
+        cada uma com água e tempere com sal grosso e uma folha de louro. Leve as 3 panelas ao 
+        fogo alto. Quando começar a ferver, deixe o trigo cozinhar por 15 minutos, a quinoa por 
+        20 minutos e a cevadinha por 30 minutos. Enquanto os grãos cozinham, prepare a vinagrete 
+        de romã (veja a receita a seguir); lave e seque o miniagrião. Quando os grãos estiverem 
+        cozidos, escorra a água. Reserve. Com uma faquinha, corte os damascos em cubos. Pique 
+        grosso as castanhas-do-pará. Numa tigela, misture os grãos já cozidos e escorridos, o 
+        damasco em cubos e as castanhas-do-pará. Aos poucos, regue com a vinagrete de romã e 
+        misture delicadamente. Leve à geladeira até a hora de servir.",
+        portion: "6",
+        duration: "2020-10-06 00:45:00",
+        cost: 55.00,
+        poster: "Salada-de-graos.jpg"
+    },
+    {
+        name: "Papilote de salmão com legumes",
+        stuff: "4 tranches (postas) de salmão sem pele (cerca de 200 g cada), 
+        4 nabos franceses, 
+        2 talos de salsão, 
+        1 xícara (chá) de erva-doce fatiada, 
+        8 minicenouras, 
+        4 colheres (sopa) de cebolinha picada, 
+        2 ramos de endro (dill) picados e 4 ramos para decorar, 
+        caldo de 2 limões-sicilianos e 
+        azeite para untar",
+        calories: 290,
+        kind: "Peixes",
+        prepare_mode: "Preaqueça o forno a 220ºC (temperatura alta). Corte 4 corações grandes 
+        de papel-manteiga, sendo que a metade do coração deve medir o dobro do filé de salmão. 
+        Pincele o azeite no papel. Se preferir, faça o papilote com papel-alumínio. Lave bem os 
+        vegetais com uma escova. Se preferir, descasque. Numa tábua, corte os nabos e a cenoura 
+        ao meio no sentido do comprimento. Corte o salsão na diagonal, em pedaços de 3 cm. Fatie 
+        a erva-doce fino e pique a cebolinha. Pique as folhas de endro até completar 2 colheres 
+        (sopa). Junte todos os legumes e ervas picadas, salvo os raminhos de endro, e misture bem. 
+        Tempere os filés de salmão com sal e pimenta dos dois lados. Divida a mistura de legumes 
+        em 4 partes iguais. No centro de uma das metades do papel-manteiga preparado (ou do 
+        papel-alumínio), coloque 1 porção de legumes. Coloque o salmão sobre os legumes e disponha 
+        um ramo de endro. Dobre a outra metade juntando as extremidades. Feche o embrulho 
+        dobrando as bordas. Antes de fechar completamente, despeje cuidadosamente o caldo de
+        ½ limão no papilote. Repita o procedimento com o restante do salmão. Transfira os papilotes 
+        para uma assadeira e leve ao forno preaquecido para assar por 10 minutos ou até o embrulho 
+        ficar inflado e dourado nas pontas (caso esteja usando papel-manteiga). Retire do forno e 
+        transfira para 4 pratos, para que cada pessoa abra o seu papilote. Se preferir, abra e 
+        transfira o conteúdo para os pratos.",
+        portion: "4",
+        duration: "2020-10-06 00:55:00",
+        cost: 75.00,
+        poster: "Salmao-ao-forno.jpg"
+    },
+    {
+        name: "Strogonoff de frango",
+        stuff: "1 peito de frango sem pele nem osso (cerca de 700 g), 
+        200 g de cogumelos-de-paris, 
+        1 cebola, 
+        2 dentes de alho, 
+        1 ½ colher (sopa) de extrato de tomate, 
+        1 ½ colher (sopa) de ketchup caseiro, 
+        1 ½ colher (sopa) de molho inglês, 
+        1 ½ colher (sopa) de conhaque, 
+        1 ½ xícara (chá) de creme de leite fresco, 
+        azeite a gosto e 
+        sal e pimenta-do-reino moída na hora a gosto",
+        calories: 310,
+        kind: "Carnes",
+        prepare_mode: "Corte o peito de frango em cubos de 2,5 cm, transfira para uma tigela e 
+        mantenha em temperatura ambiente enquanto prepara o restante dos ingredientes – se o 
+        frango estiver gelado na hora de ir para a panela, vai cozinhar em vez de dourar.
+        Corte os cogumelos-de-paris em 3 fatias (os menores corte apenas ao meio). Descasque e 
+        pique fino a cebola e os dentes de alho. 
+        Leve ao fogo médio uma panela média. Quando aquecer, regue com 1 colher (sopa) de azeite 
+        doure os cubos de frango em etapas, sem amontoar – para que fiquem dourados. Tempere com 
+        sal e vire com a pinça para dourar os cubos por igual. Transfira para uma tigela e repita 
+        com o restante, regando a panela com azeite a cada leva. 
+        Abaixe o fogo da panela e regue com mais 1 colher (sopa) de azeite. Adicione a cebola, 
+        tempere com uma pitada de sal e refogue até murchar, por 3 minutos – aproveite para ir 
+        raspando o fundo da panela, dissolvendo os queimadinhos que são essenciais para dar sabor 
+        ao preparo. Junte o alho e mexa por 1 minuto para perfumar. 
+        Acrescente o extrato de tomate, o ketchup e mexa bem. Volte o frango dourado para a panela, 
+        junte o conhaque e mexa para evaporar o álcool. Junte o molho inglês, o creme de leite e 
+        misture bem. Tempere com sal e pimenta e deixe cozinhar em fogo médio por cerca de 10 
+        minutos, mexendo de vez em quando, até engrossar. 
+        Acrescente os cogumelos e deixe cozinhar por mais 2 minutos até ficarem levemente macios. 
+        Sirva a seguir, com arroz e batata-doce palha.",
+        portion: "4",
+        duration: "2020-10-06 01:00:00",
+        cost: 58.00,
+        poster: "Strogonoff-de-Frango-Cremoso.jpg"
+    },
+    {
+        name: "Torta Madalena",
+        stuff: "Para a massa: 1 kg de batata asterix (cerca de 6 unidades), 
+        1 ¼ de xícara (chá) de leite, 
+        ¼ de xícara (chá) de queijo parmesão ralado fino, 
+        2 colheres (sopa) de manteiga, 
+        1 louro, 
+        noz-moscada ralada na hora a gosto e 
+        sal a gosto. Para o recheio: 500 g de patinho moído, 
+        1 cebola, 
+        1 cenoura, 
+        2 dentes de alho, 
+        3 filés de anchovas, 
+        ½ xícara (chá) de ervilha congelada (cerca de 80 g), 
+        2 colheres (sopa) de extrato de tomate, 
+        2 colheres (sopa) de farinha de trigo, 
+        2 xícaras (chá) da água do cozimento das batatas, 
+        1 colher (sopa) de shoyu (molho de soja), 
+        1 colher (sopa) de vinagre de vinho tinto, 
+        2 ramos de tomilho, 
+        1 pitada de cravo em pó, 
+        azeite a gosto, 
+        sal e pimenta-do-reino moída na hora a gosto e 
+        queijo parmesão ralado fino a gosto, para polvilhar.",
+        calories: 270,
+        kind: "Carnes",
+        prepare_mode: "MASSA - Descasque, corte as batatas em pedaços médios e transfira para uma panela. 
+        Junte 1 colher (chá) de sal, a folha de louro, cubra com água e leve ao fogo alto. Assim 
+        que ferver, diminua o fogo para médio e deixe cozinhar por cerca de 20 minutos até ficarem 
+        macias - espete com um garfo para verificar. Enquanto isso, aproveite para separar os 
+        ingredientes do recheio.
+        Assim que as batatas estiverem cozidas, desligue o fogo e reserve 2 xícaras (chá) da água do 
+        cozimento para utilizar no recheio. Passe as batatas pelo escorredor e deixe por alguns 
+        minutos para secar. Enquanto isso, coloque o leite numa jarra de vidro e leve para rodar no 
+        micro-ondas por cerca de 1 minuto para aquecer - esse é o truque para o purê não empelotar.
+        Sobre a panela seca, passe as batatas ainda quentes pelo espremedor. Volte ao fogo médio e 
+        acrescente o leite aquecido aos poucos, mexendo com o batedor de arame para ficar cremoso. 
+        Junte a manteiga e misture até derreter. Desligue o fogo e misture o queijo ralado. Tempere 
+        com noz-moscada a gosto, prove e ajuste o sal. Transfira para uma tigela e reserve. 
+        RECHEIO - Preaqueça o forno a 200 ºC (temperatura média). Com um pedaço de papel toalha, 
+        espalhe uma camada bem fina de azeite num refratário de cerca de 30 cm x 20 cm. 
+        Descasque e passe a cenoura pela parte fina do ralador. Descasque e pique fino a cebola e o 
+        alho. Leve ao fogo médio uma panela média para aquecer. Regue com 1 colher (sopa) de azeite 
+        e junte a carne moída. Tempere com uma pitada de sal e polvilhe com a farinha de trigo. Deixe 
+        cozinhar por cerca de 10 minutos até a farinha dourar e a carne cozinhar - mexa de vez em 
+        quando com uma espátula para soltar a carne moída.
+        Transfira a carne dourada para uma tigela e mantenha a panela em fogo médio. Regue com mais ½ 
+        colher (sopa) de azeite e acrescente a cebola. Tempere com sal e refogue por cerca de 1 minuto 
+        até murchar. Junte a cenoura ralada e refogue por mais 2 minutos para murchar. Adicione o alho, 
+        o anchova e mexa por 1 minuto.
+        Junte à panela o extrato de tomate e misture bem. Regue com a água e mexa com a espátula, 
+        raspando o fundo da panela para dissolver os queimadinhos. Acrescente o shoyu, o vinagre, o 
+        cravo em pó e deixe cozinhar até ferver.
+        Assim que ferver, volte a carne para a panela. Junte a ervilha, o tomilho debulhado e misture. 
+        Diminua o fogo e deixe cozinhar por cerca de 10 minutos, mexendo de vez em quando para não grudar 
+        no fundo da panela, até o recheio encorpar.
+        Transfira o recheio para o refratário untado com azeite e cubra com o purê de batatas reservado - 
+        espalhe com as costas de uma colher para cobrir toda a carne. Com um garfo, raspe a superfície do 
+        purê para marcar e polvilhe com queijo ralado a gosto - assim ele fica bem rústico para gratinar.
+        Leve o refratário ao forno e deixe assar por cerca de 20 minutos até dourar. Retire do forno e 
+        sirva a torta a seguir.",
+        portion: "5",
+        duration: "2020-10-06 00:35:00",
+        cost: 56.00,
+        poster: "Torta-madalena.jpg"
+    }
+])
