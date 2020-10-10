dd3a18f0c7bbb51898eae6e2366760110f111517
paulanguedes <paulanguedes@gmail.com>
2020-10-10 20:36:48 -0300

    creating flashes notice and alert

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,11 +15,10 @@ class RecipesController < ApplicationController
     def update
         @recipe = Recipe.find(params[:id])
         if @recipe.update(recipe_params)
-            redirect_to @recipe
+            redirect_to @recipe, notice:"Sua receita foi editada."
         else
             render :edit
         end
-        redirect_to @recipe
     end
 
     def new
@@ -29,17 +28,16 @@ class RecipesController < ApplicationController
     def create
         @recipe = Recipe.new(recipe_params)
         if @recipe.save
-            redirect_to @recipe
+            redirect_to @recipe, notice:"Sua receita foi criada com sucesso."
         else
             render :new
         end
-        
     end
     
     def destroy
         @recipe = Recipe.find(params[:id])
         @recipe.destroy
-        redirect_to recipes_url
+        redirect_to recipes_url, alert:"Receita excluída :("
     end
 
     private 
