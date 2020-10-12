ad8f3e7d3cd5b45b4d19899bf1713069b2193b11
paulanguedes <paulanguedes@gmail.com>
2020-10-12 17:40:38 -0300

    comments controller ajustments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,16 @@
 class CommentsController < ApplicationController
 
+    before_action :set_recipe
+
     def index
-        @recipe = Recipe.find(params[:recipe_id])
         @comments = @recipe.comments
     end
 
     def new
-        @recipe = Recipe.find(params[:recipe_id])
         @comment = @recipe.comments.new
     end
 
     def create
-        @recipe = Recipe.find(params[:recipe_id])
         @comment = @recipe.comments.new(comment_params)
         if @comment.save
             redirect_to recipe_comments_path(@recipe), notice: "Avaliação enviada! Obrigada"
@@ -24,6 +23,10 @@ class CommentsController < ApplicationController
 
         def comment_params
             params.require(:comment).permit(:name, :comment, :rating)
+        end
+
+        def set_recipe
+            @recipe = Recipe.find(params[:recipe_id])
         end
 
 end
