ca0f4c66aae8790c4c520707bf3f7f769bf8f0c0
paulanguedes <paulanguedes@gmail.com>
2020-10-12 17:30:11 -0300

    comments: view, controller, validation, form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,4 +4,26 @@ class CommentsController < ApplicationController
         @recipe = Recipe.find(params[:recipe_id])
         @comments = @recipe.comments
     end
+
+    def new
+        @recipe = Recipe.find(params[:recipe_id])
+        @comment = @recipe.comments.new
+    end
+
+    def create
+        @recipe = Recipe.find(params[:recipe_id])
+        @comment = @recipe.comments.new(comment_params)
+        if @comment.save
+            redirect_to recipe_comments_path(@recipe), notice: "Avaliação enviada! Obrigada"
+        else
+            render :new
+        end
+    end
+
+    private
+
+        def comment_params
+            params.require(:comment).permit(:name, :comment, :rating)
+        end
+
 end
